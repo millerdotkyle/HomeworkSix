a5224202ce2931173fe17337daadbd7559c8f909
millerdotkyle <kylenotfound@gmail.com>
2023-06-26 18:22:54 -0400

    saved
    -imported, saved work in progress for week 6 hw

--- assets/script.js
+++ assets/script.js
@@ -18,18 +18,51 @@ function getWeather() {
         return response.json();
     })
     .then(function(data){
-        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=" + key)
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=" + key + "&units=imperial")
         .then(function(response) {
             return response.json();
         })
         .then(function (data) {
             // console.log(data);
-            for(i=0; i<5; i++) {
-                var a = i * 8;
-                console.log(data.list[a]);
-            }
+            todayWeather(data);
+            printForecast(data);
         })
     })
 }
 
 searchBtn.addEventListener("click", getWeather);
+
+function todayWeather(data) {
+    console.log(data);
+    var today = dayjs().format('MMM D, YYYY');
+
+    $('#today-city').text('City: ' + data.city.name + ' ' + today);
+    $('#today-temp').text('Temp: ' + data.list[0].main.temp + '*F');
+    $('#today-wind').text('Wind: ' + data.list[0].wind.speed + 'MPH');
+    $('#today-humidity').text('Humidity: ' + data.list[0].main.humidity +'%');
+}
+
+function printForecast (data) {
+    var cardEl = $('#forecast');
+
+    for(i=0; i<5; i++) {
+        a = i*8;
+
+        var newcast = [dayjs(data.list[a].dt_txt).format('dddd'), data.list[a].main.temp, data.list[a].wind.speed, data.list[a].main.humidity];
+
+        var div = $("<div>");
+        var ul = $('<ul>');
+        $.each(newcast, function(i, item) {
+            let li = $("<li>");
+            li.text(item);
+            li.addClass('list-group-item');
+            ul.append(li);
+        });
+
+        ul.addClass('list-group list-group-flush');
+        div.addClass('col');
+        div.append(ul);
+        cardEl.append(div);
+    }
+
+}
