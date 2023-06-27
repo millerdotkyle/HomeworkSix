22443599c183cbe43c26c407c62f6b678cf9629f
millerdotkyle <kylenotfound@gmail.com>
2023-06-27 02:09:49 -0400

    updated
    -most logic completed
    -style and layout mostly accurate from bootstrap classes and custom CSS

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
 var key = "9fbe4849e7907051c07050bc1201336d";
-// var today = dayjs();
+
+
 
 
 var searchesDisplayEl = document.getElementById("searches");
 var searchBtn = document.getElementById("search-button");
 
 
-var prevSearches =localStorage.getItem("savedsearches") || [];
+var prevSearches = JSON.parse(localStorage.getItem("savedsearches")) || [];
+
 
 
 var currentCity = $('input[name="City"]');
 
+printSavedCities(prevSearches);
+
 
 function getWeather() {
+    saveCity(prevSearches);
+    printSavedCities(prevSearches);
     fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + currentCity.val() + "&limit=1&appid=" + key)
     .then(function(response) {
         return response.json();
@@ -23,7 +29,6 @@ function getWeather() {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data);
             todayWeather(data);
             printForecast(data);
         })
@@ -34,35 +39,71 @@ searchBtn.addEventListener("click", getWeather);
 
 function todayWeather(data) {
     console.log(data);
-    var today = dayjs().format('MMM D, YYYY');
+    var today = dayjs().format('M/DD/YYYY');
 
-    $('#today-city').text('City: ' + data.city.name + ' ' + today);
-    $('#today-temp').text('Temp: ' + data.list[0].main.temp + '*F');
+    $('#today-city').text(data.city.name + ' (' + today + ')');
+    $('#today-icon').attr('src', 'https://openweathermap.org/img/wn/'+ data.list[0].weather[0].icon + '.png');
+    $('#today-temp').text('Temp: ' + data.list[0].main.temp + '°F');
     $('#today-wind').text('Wind: ' + data.list[0].wind.speed + 'MPH');
     $('#today-humidity').text('Humidity: ' + data.list[0].main.humidity +'%');
 }
 
 function printForecast (data) {
     var cardEl = $('#forecast');
+    cardEl.text("");
 
-    for(i=0; i<5; i++) {
+    for(var i=0; i<5; i++) {
         a = i*8;
 
-        var newcast = [dayjs(data.list[a].dt_txt).format('dddd'), data.list[a].main.temp, data.list[a].wind.speed, data.list[a].main.humidity];
+        newIcon = data.list[0].weather[0].icon;
+        var newcast = [dayjs(data.list[a].dt_txt).format('DD/MM/YYYY'), ' ', data.list[a].main.temp, data.list[a].wind.speed, data.list[a].main.humidity];
+        var units = [' ', ' ', '°F', 'MPH', '%']
 
         var div = $("<div>");
         var ul = $('<ul>');
-        $.each(newcast, function(i, item) {
-            let li = $("<li>");
-            li.text(item);
-            li.addClass('list-group-item');
-            ul.append(li);
-        });
+
+        for(var j =0; j<newcast.length; j++) {
+            if(j===1) {
+                var img = $('<img>');
+                img.attr('src', 'https://openweathermap.org/img/wn/' + data.list[a].weather[0].icon + '.png');
+                img.attr('class', 'icon');
+                ul.append(img);
+            } else{
+                let li = $("<li>");
+                li.text(newcast[j] + ' ' + units[j]);
+                li.addClass('list-group-item');
+    
+                ul.append(li);
+            }
+
+        }
+
+
 
         ul.addClass('list-group list-group-flush');
-        div.addClass('col');
+        div.addClass('col day-card pl-2 pr-2');
         div.append(ul);
         cardEl.append(div);
     }
 
+}
+
+function printSavedCities (arr) {
+
+
+    var ulEl = $('#saved-cities');
+    ulEl.children().remove();
+    for(let i=0; i<arr.length; i++) {
+        var li = $('<li>');
+        li.text(arr[i]);
+        li.addClass('btn bg-secondary mt-2 mb-2');
+        ulEl.append(li);
+    }
+    ulEl.removeClass('hidden');
+
+}
+
+function saveCity (prevSearches) {
+    prevSearches.push($('input').val());
+    localStorage.setItem('savedsearches', JSON.stringify(prevSearches));
 }
